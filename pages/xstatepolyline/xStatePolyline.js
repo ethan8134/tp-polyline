0bf5ed84781e300dd741098dae378f423a21ea68
ethan8134 <ethan.cabanes@etud.univ-jfc.fr>
2025-09-22 15:09:25 +0200

    exercice polyline complété

--- pages/xstatepolyline/xStatePolyline.js
+++ pages/xstatepolyline/xStatePolyline.js
@@ -1,7 +1,6 @@
 import Konva from "konva";
 import { createMachine, interpret } from "xstate";
 
-
 const stage = new Konva.Stage({
     container: "container",
     width: 400,
@@ -12,17 +11,56 @@ const layer = new Konva.Layer();
 stage.add(layer);
 
 const MAX_POINTS = 10;
-let polyline // La polyline en cours de construction;
+let polyline; // La polyline en cours de construction;
 
 const polylineMachine = createMachine(
     {
-        /** @xstate-layout N4IgpgJg5mDOIC5gF8A0IB2B7CdGgAcsAbATwBkBLDMfEI2SgF0qwzoA9EBaANnVI9eAOgAM4iZMkB2ZGnokK1MMMoRitJAsYs2nRABYATAMQAOAIzCD0gJwXetgwGZezgw9ty5QA */
+        /** @xstate-layout N4IgpgJg5mDOIC5QAcD2AbAngGQJYDswA6XCdMAYgFkB5AVQGUBRAYWwEkWBpAbQAYAuohSpYuAC65U+YSAAeiACwBWRUQAcyvioBMAdgCcBlQDZ1AGhCZEARkVqDOgza17DfPTZ3qAvj8toWHiERBAATgCGAO4EUNT0zLQAakz8QkggaGKS0rIKCMY6RADMJgYmfOomio56ihZWtkSKTi7q6qUmqsV8Oop+ARg4BMTh0bEUTPjiYGFpslkSUjIZ+cXORF18ZS0Gbur6ltYINs2t9srFLus2xZ4DmUPBo5Ex+HG0jKwc3PMZizkVqB8gY+HwiDotJo9Oo+MVlB1FEdEN51EQbIYbAYqso3DpSsoHoFhiExm84gAhCIAYwA1rBkDSwH8RNllnlEMV7BCtLUbDYTDcGsdFHwDJtDPpFCY9GUTGUiU8RqFXhMmLBqRFkMzBAtREtcqslG4iJ4nMo2rjFHodMiEABaA5EXGVeG9WWSvR+fwgfCoCBwPVBEZ6tmG4Eo7TNMEqLQ6bS9HQmO32gXi9R6V2OPrWrG+H3E54kMhgUMGoHyFHKIqi7TKOMJnRJu1tdGY5TODEwjGK4Ok1XvMuAjkIHZEUHbPY2gz14wthFt5wd-n7HveoA */
         id: "polyLine",
         initial: "idle",
-        states : {
+        states: {
             idle: {
-            }
-        }
+                on: {
+                    MOUSECLICK: {
+                        target: "drawing",
+                        actions: "createLine",
+                    },
+                },
+            },
+
+            drawing: {
+                on: {
+                    Enter: {
+                        target: "idle",
+                        actions: "saveLine",
+                    },
+
+                    MOUSEMOVE: {
+                        target: "drawing",
+                        actions: "setLastPoint",
+                    },
+
+                    MOUSECLICK: {
+                        target: "drawing",
+                        internal: true,
+                        actions: "addPoint",
+                        cond: "pasPlein",
+                    },
+
+                    Backspace: {
+                        target: "drawing",
+                        internal: true,
+                        actions: "removeLastPoint",
+                        cond: "plusDeDeuxPoints",
+                    },
+
+                    Escape: {
+                        target: "idle",
+                        actions: "abandon",
+                    },
+                },
+            },
+        },
     },
     // Quelques actions et guardes que vous pouvez utiliser dans votre machine
     {
@@ -67,7 +105,9 @@ const polylineMachine = createMachine(
             // Abandonner le tracé de la polyline
             abandon: (context, event) => {
                 // Supprimer la variable polyline :
-                
+                polyline.destroy();
+                polyline = null;
+                layer.batchDraw(); // Redraw the layer to reflect the changes
             },
             // Supprimer le dernier point de la polyline
             removeLastPoint: (context, event) => {
@@ -84,7 +124,7 @@ const polylineMachine = createMachine(
             pasPlein: (context, event) => {
                 // Retourner vrai si la polyline a moins de 10 points
                 // attention : dans le tableau de points, chaque point est représenté par 2 valeurs (coordonnées x et y)
-                
+                return polyline.points().length < MAX_POINTS * 2 + 2;
             },
             // On peut enlever un point
             plusDeDeuxPoints: (context, event) => {
